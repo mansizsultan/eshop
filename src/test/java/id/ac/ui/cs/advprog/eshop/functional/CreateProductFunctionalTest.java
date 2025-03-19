ebb4adfbb7ae35474aa1c299c58c54665e5216ce
mansizsultan <sultanibnumansiz@gmail.com>
2025-03-19 09:01:36 +0700

    Modify CreateProductFunctionalTest to reflect test adjustments

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -1,4 +1,51 @@
 package id.ac.ui.cs.advprog.eshop.functional;
 
-public class CreateProductFunctionalTest {
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void userCanCreateProduct(ChromeDriver driver) {
+        driver.get(baseUrl + "/product/create");
+
+        WebElement nameInput = driver.findElement(By.name("productName"));
+        WebElement quantityInput = driver.findElement(By.name("productQuantity"));
+        WebElement submitButton = driver.findElement(By.tagName("button"));
+
+        nameInput.sendKeys("GTA VI");
+        quantityInput.sendKeys("1");
+        submitButton.click();
+
+        driver.get(baseUrl + "/product/list");
+
+        String pageSource = driver.getPageSource();
+        assertTrue(pageSource.contains("GTA VI"));
+        assertTrue(pageSource.contains("1"));
+    }
 }
